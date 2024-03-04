e9384227a05495bea2b413619b0074dc66037931
JesusConejo <jconejo70@gmail.com>
2024-03-04 17:15:58 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.galeria',
+  appName: 'galeria',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
